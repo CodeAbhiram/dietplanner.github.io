aca69ec79b7039ff1a790726f720a8d83fd19cf1
Abhiram Sanapala <sanapalaabhiram58@gmail.com>
2024-07-13 21:57:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -86,4 +86,16 @@ document.getElementById("submit").addEventListener('click', function(e) {
         console.error('Error:', error);
       });
   });
+  document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const menu = document.querySelector('.menu');
+  
+    menuToggle.addEventListener('change', function() {
+      if (menuToggle.checked) {
+        menu.style.display = 'flex';
+      } else {
+        menu.style.display = 'none';
+      }
+    });
+  });
   
